8a33671cee95dee45cc6e8d1c80514971fbbf230
junyongk <aseagullb@naver.com>
2017-07-19 11:03:12 +0900

    Update test.java

--- src/com/moon/DnsChanger/test.java
+++ src/com/moon/DnsChanger/test.java
@@ -33,7 +33,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		passwd="test123!@#";
 		
 		edns1= (EditText)findViewById(R.id.edns1);
 		edns2= (EditText)findViewById(R.id.edns2);
